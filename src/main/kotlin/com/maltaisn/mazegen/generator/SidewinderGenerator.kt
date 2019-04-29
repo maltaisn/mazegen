495c48ca874225f5b28db3d68f2113cdde53e528
maltaisn <nicolas.g.maltais@gmail.com>
2019-04-28 20:27:00 -0400

    Updated copyright

--- src/main/kotlin/com/maltaisn/mazegen/generator/SidewinderGenerator.kt
+++ src/main/kotlin/com/maltaisn/mazegen/generator/SidewinderGenerator.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Nicolas Maltais
+ * Copyright (c) 2019 Nicolas Maltais
  *
  * Permission is hereby granted, free of charge,
  * to any person obtaining a copy of this software and
